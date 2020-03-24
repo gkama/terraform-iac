8f07448a1a54d1e858520cd4e4fdb6ac78a5409e
Georgi Kamacharov <gkamacharov@gmail.com>
2020-03-24 17:09:25 -0400

    update azurerm provider version

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ variable client-secret {}
 variable location {}
 
 provider "azurerm" {
- version = "=2.0.0"
+ version = "~> 2.0.0"
  features {}
  subscription_id = var.subscription-id
  tenant_id = var.tenant-id
