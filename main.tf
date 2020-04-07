7fed71de2a6805325f444ca25dd83b8308512cba
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-07 16:52:50 -0400

    add app service plan

--- main.tf
+++ main.tf
@@ -57,3 +57,21 @@ resource "azurerm_container_registry" "gkamacr" {
     environment = var.environment
   }
 }
+
+
+resource "azurerm_app_service_plan" "gkama-service-plan" {
+  name                = "gkama-service-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  kind                = "Linux"
+  reserved            = false
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+
+  tags                     = {
+    environment = var.environment
+  }
+}
