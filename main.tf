c3aa8820d20350057720db52a49322e46c64f471
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-07 17:26:41 -0400

    update reserved to true

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "azurerm_app_service_plan" "gkama-service-plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   kind                = "Linux"
-  reserved            = false
+  reserved            = true
 
   sku {
     tier = "Basic"
