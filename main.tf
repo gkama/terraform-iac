b07826ecfc3c5c6e4e0abf8eb8484e661affa3dd
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-06 23:20:38 -0400

    add service fabric

--- main.tf
+++ main.tf
@@ -13,11 +13,28 @@ provider "azurerm" {
  client_secret = var.client_secret
 }
 
-locals {
-  resource_group_name = "terraform_iac"
+
+resource "azurerm_resource_group" "terraform-iac" {
+ name     = "terraform-iac"
+ location = var.location
 }
 
-resource "azurerm_resource_group" "rg" {
- name     = local.resource_group_name
- location = var.location
+
+resource "azurerm_service_fabric_cluster" "gkama-servicefabric" {
+  name                 = "gkama-servicefabric"
+  resource_group_name  = azurerm_resource_group.terraform-iac.name
+  location             = azurerm_resource_group.example.location
+  reliability_level    = "Bronze"
+  upgrade_mode         = "Manual"
+  cluster_code_version = "6.5.639.9590"
+  vm_image             = "Linux"
+  management_endpoint  = "https://example:80"
+
+  node_type {
+    name                 = "first"
+    instance_count       = 3
+    is_primary           = true
+    client_endpoint_port = 2020
+    http_endpoint_port   = 80
+  }
 }
